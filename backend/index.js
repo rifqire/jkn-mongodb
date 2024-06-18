80c890d878c16f131c21ea80df62cc70b36fc29b
Rifqi Rachmanda Eryawan <rifqi30rachmanda@gmail.com>
2024-06-18 09:15:33 +0700

    Set mongodb url, create & connect database

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+mongoose.connect('mongodb://localhost:27017/jkn_db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Database connected'))
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000, () => console.log('Server is running...'))
