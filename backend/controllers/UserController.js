8c17edae736f7957b891449984cbaec9ce4881b5
Rifqi Rachmanda Eryawan <rifqi30rachmanda@gmail.com>
2024-06-18 10:50:19 +0700

    Add update & delete functions

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,66 @@
 import User from '../models/UserModel.js'
 
-// Function to find users based on ID
-// If error, show error message
-export const getUsers = async (req, res) => {
+// Function to save data
+// Create new user instance from model and use request body to send data to db
+// If successful, show status 201 (created)
+// If error, show error 400 (user's fault)
+export const saveUser = async (req, res) => {
+    const user = new User(req.body)
+    try {
+        const savedUser = await user.save()
+        res.status(201).json(savedUser)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+// Function to find all users
+// If error, show error 500 message (internal server error)
+export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find()
         res.json(users)
     } catch (error) {
         res.status(500).json({message: error.message})
+    }
+}
+
+// Function to find selected user based on ID
+// If error, show error 404 message (not found)
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+// Function to update user data
+// If successful, show status 200 (ok)
+// If error, show error 400 (user's fault)
+export const updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.updateOne(
+            {_id: req.params.id},
+            {$set: req.body}
+        )
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+// Function to delete user
+// If successful, show status 200 (ok)
+// If error, show error 400 (user's fault)
+export const deleteUser = async (req, res) => {
+    try {
+        const deletedUser = await User.deleteOne(
+            {_id: req.params.id}
+        )
+        res.status(200).json(deletedUser)
+    } catch (error) {
+        res.status(400).json({message: error.message})
     }
 }
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,24 @@
 import express from 'express'
-// Import function getUsers from UserController
-import { getUsers } from '../controllers/UserController.js'
+// Import CRUD functions from UserController
+import {
+    saveUser,
+    getUserById,
+    getAllUsers,
+    updateUser,
+    deleteUser
+} from '../controllers/UserController.js'
 
 const router = express.Router()
 
-router.get('/users', getUsers)
+// Router to create user data
+router.post('/users', saveUser)
+// Router to get selected user by ID
+router.get('/users/:id', getUserById)
+// Router to get all users
+router.get('/users', getAllUsers)
+// Router to update selected user's data (use method patch)
+router.patch('/users/:id', updateUser)
+// Router to delete user by ID
+router.delete('/users/:id', deleteUser)
 
 export default router
