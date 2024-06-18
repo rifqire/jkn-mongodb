a7bc2af9937ad29166fe964c00e9607250e857ec
Rifqi Rachmanda Eryawan <rifqi30rachmanda@gmail.com>
2024-06-18 13:45:18 +0700

    Add delete user by id function in UserList component

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,29 +1,40 @@
 // Command rafce, then import useState and useEffect
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 // Import axios to fetch data from API
-import axios from "axios";
+import axios from "axios"
 // Import "link" to go to another page after button click
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 // Function to view all users
 const UserList = () => {
   // Create new states
-  const [users, setUser] = useState([]);
+  const [users, setUser] = useState([])
 
   // Use effect and call method getAllUsers
   useEffect(() => {
-    getAllUsers();
-  }, []);
+    getAllUsers()
+  }, [])
 
   // Function to get all user data from API with axios
   const getAllUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
+    const response = await axios.get("http://localhost:5000/users")
     // Display all users in console
     // console.log(response.data)
 
     // Display all users in the table with setUser function
-    setUser(response.data);
-  };
+    setUser(response.data)
+  }
+
+  // Create function to delete user
+  // If successfull, call function to get all users
+  const deleteUser = async (id) => {
+    try {
+        await axios.delete(`http://localhost:5000/users/${id}`)
+        getAllUsers()
+    } catch (error) {
+        console.log(error)
+    }
+  }
 
   return (
     <div className="columns">
@@ -58,14 +69,20 @@ const UserList = () => {
                 >
                   Edit
                 </Link>
-                <button className="button is-danger is-small">Delete</button>
+                {/* Use deleteUser function on click */}
+                <button
+                  onClick={() => deleteUser(user._id)}
+                  className="button is-danger is-small"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default UserList;
+export default UserList
